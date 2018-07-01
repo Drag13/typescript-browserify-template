53539e85cf0714042ea8827ca9503f15bcd4b99c
Vitalii Drag <ruban.vitaliy@gmail.com>
2018-07-01 22:03:26 +0300

    npm commands renaming

--- server/app/www.ts
+++ server/app/www.ts
@@ -17,7 +17,7 @@ server.on('listening', onListening);
 /**
  * Normalize a port into a number, string, or false.
  */
-function normalizePort(val: any): number|string|boolean {
+function normalizePort(val: any): number | string | boolean {
   let port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -36,7 +36,7 @@ function normalizePort(val: any): number|string|boolean {
 /**
  * Event listener for HTTP server "error" event.
  */
-function onError(error) {
+function onError(error: any) {
   if (error.syscall !== 'listen') {
     throw error;
   }
